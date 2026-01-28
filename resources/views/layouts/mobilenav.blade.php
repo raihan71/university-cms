d316840850200aa8aa8bb70bf1c92e03fad8dc43
Raihan Nismara <raihan@d3labs.io>
2026-01-28 19:51:34 +0700

    add: ui design page, intial laravel 10

---
+++ resources/views/layouts/mobilenav.blade.php
@@ -0,0 +1,89 @@
+<div class="mobile-menu-area">
+  <div class="container">
+      <div class="row">
+          <div class="col-md-12">
+              <div class="mobile-menu">
+                  <nav id="dropdown">
+                      <ul>
+                          <li><a href="#">Home</a>
+                              <ul>
+                                  <li><a href="index-2.html">Home 1</a></li>
+                                  <li><a href="index2.html">Home 2</a></li>
+                                  <li><a href="index3.html">Home 3</a></li>
+                                  <li><a href="index4.html">Home 4</a></li>
+                              </ul>
+                          </li>
+                          <li><a href="#">Pages</a>
+                              <ul>
+                                  <li><a href="about1.html">About 1</a></li>
+                                  <li><a href="about2.html">About 2</a></li>
+                                  <li><a href="about3.html">About 3</a></li>
+                                  <li><a href="about4.html">About 4</a></li>
+                                  <li><a href="lecturers1.html">lecturers 1</a></li>
+                                  <li><a href="lecturers2.html">lecturers 2</a></li>
+                                  <li><a href="single-lecturers.html">lecturers Details</a></li>
+                                  <li><a href="pricing1.html">Pricing Plan 1</a></li>
+                                  <li><a href="pricing2.html">Pricing Plan 2</a></li>
+                                  <li><a href="shop1.html">Shop 1</a></li>
+                                  <li><a href="shop2.html">Shop 2</a></li>
+                                  <li><a href="single-shop.html">Shop Details</a></li>
+                                  <li><a href="account.html">Account</a></li>
+                                  <li><a href="registration.html">Registration</a></li>
+                                  <li><a href="faq.html">Faq</a></li>
+                                  <li><a href="404.html">404 Error</a></li>
+                              </ul>
+                          </li>
+                          <li><a href="#">Courses</a>
+                              <ul>
+                                  <li><a href="courses1.html">Courses 1</a></li>
+                                  <li><a href="courses2.html">Courses 2</a></li>
+                                  <li><a href="courses3.html">Courses 3</a></li>
+                                  <li><a href="single-courses1.html">Course Details 1</a></li>
+                                  <li><a href="single-courses2.html">Course Details 2</a></li>
+                                  <li><a href="single-courses3.html">Course Details 3</a></li>
+                              </ul>
+                          </li>
+                          <li><a href="#">Research</a>
+                              <ul>
+                                  <li><a href="research1.html">Research 1</a></li>
+                                  <li><a href="research2.html">Research 2</a></li>
+                                  <li><a href="research3.html">Research 3</a></li>
+                                  <li><a href="single-research.html">Research Details</a></li>
+                              </ul>
+                          </li>
+                          <li><a href="#">News</a>
+                              <ul>
+                                  <li class="has-child-menu"><a href="#">News</a>
+                                      <ul class="thired-level">
+                                          <li><a href="news1.html">News 1</a></li>
+                                          <li><a href="news2.html">News 2</a></li>
+                                          <li><a href="single-news.html">News Details</a></li>
+                                      </ul>
+                                  </li>
+                                  <li class="has-child-menu"><a href="#">Event</a>
+                                      <ul class="thired-level">
+                                          <li><a href="event.html">Event</a></li>
+                                          <li><a href="single-event.html">Event Details</a></li>
+                                      </ul>
+                                  </li>
+                              </ul>
+                          </li>
+                          <li><a href="#">Gallery</a>
+                              <ul>
+                                  <li><a href="gallery1.html">Gallery 1</a></li>
+                                  <li><a href="gallery2.html">Gallery 2</a></li>
+                              </ul>
+                          </li>
+                          <li><a href="#">Contact</a>
+                              <ul>
+                                  <li><a href="contact1.html">Contact 1</a></li>
+                                  <li><a href="contact2.html">Contact 2</a></li>
+                              </ul>
+                          </li>
+                      </ul>
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
